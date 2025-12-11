fefc071119420791eae2a0482b8d2076a0d4fa98
malachisor1103 <malachisor1103@gmail.com>
2025-12-10 23:27:57 -0500

    debug is now false

--- TicketMasterProject/settings.py
+++ TicketMasterProject/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -101,7 +101,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LANGUAGE_CODE = 'en-us'
 
